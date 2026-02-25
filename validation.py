0d6cab6bd88d7f52081c74828997d55ce0292eef
ebat-Deadins <e.batbileg0506@gmail.com>
2026-02-25 20:04:34 +0800

    Add input validation logic in validation.py
    This file contains validation logic for input fields, ensuring they are not empty, that age is a number, and that the Student ID format is correct.

---
+++ validation.py
@@ -0,0 +1,13 @@
+validation.py → Input Checking
+
+This file makes sure:
+
+Fields are not empty
+
+Age is a number
+
+Student ID format is correct
+
+⚠️ No GUI code
+⚠️ No file saving
+Only validation logic
