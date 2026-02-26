dd27e7d8d9c839015550b84b64c2602d7163d9aa
ebat-Deadins <e.batbileg0506@gmail.com>
2026-02-26 17:17:09 +0800

    Update main_guy.py

--- main_guy.py
+++ main_guy.py
@@ -1,5 +1,7 @@
 import tkinter
 from tkinter import messagebox
+import data_manager
+
 def main_window():
     main_win = tkinter.Tk()
     main_win.title("Main Window")
@@ -9,6 +11,11 @@ def main_window():
     welcome_label = tkinter.Label(
         main_win, text="Welcome to the Main Window!", bg='#333333', fg="#FF3399", font=("Arial", 20))
     welcome_label.pack(pady=100)
-
+    frame = tkinter.Frame(bg='#333333')
+    students_label = tkinter.Label(
+    frame, text="Students", bg='#333333', fg="#FFFFFF", font=("Arial", 16))
+    students_label.grid(row=2, column=0, pady=20)
+    frame.pack()
     main_win.mainloop()
+
 
